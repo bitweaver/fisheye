31cd95eaa1359a33a637cda2fd0bfe3068115a4e
Max Kremmel <xing@synapse.plus.com>
2005-06-21 18:17:51 +0000

    limit sort_mode settings to avoid database errors on wrong entry

--- modules/mod_images.php
+++ modules/mod_images.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_fisheye/modules/mod_images.php,v 1.1 2005/06/21 10:24:40 squareing Exp $
+// $Header: /cvsroot/bitweaver/_bit_fisheye/modules/mod_images.php,v 1.2 2005/06/21 18:17:51 squareing Exp $
 global $gQueryUserId, $module_rows, $module_params;
 
 require_once( FISHEYE_PKG_PATH.'FisheyeImage.php' );
@@ -9,7 +9,15 @@ $image = new FisheyeImage();
 $listHash = $module_params;
 $listHash['max_records'] = $module_rows;
 $listHash['user_id'] = $gQueryUserId;
-$listHash['sort_mode'] = !empty( $module_params['sort_mode'] ) ? $module_params['sort_mode'].'_asc' : 'random';
+
+// this is needed to avoid wrong sort_modes entered resulting in db errors
+$sort_options = array( 'hits', 'created' );
+if( !empty( $module_params['sort_mode'] ) && in_array( $module_params['sort_mode'], $sort_options ) ) {
+	$sort_mode = $module_params['sort_mode'].'_asc';
+} else {
+	$sort_mode = 'random';
+}
+$listHash['sort_mode'] = $sort_mode;
 
 $images = $image->getList( $listHash );
 
