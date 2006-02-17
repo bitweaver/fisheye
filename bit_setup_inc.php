5bda6ec56941b93a3d4e6d215c929825fb801b0f
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:41 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,20 +1,24 @@
 <?php
 global $gBitSystem, $gBitSmarty;
 
-$gBitSystem->registerPackage( 'fisheye', dirname( __FILE__).'/' );
+$registerHash = array(
+	'package_name' => 'fisheye',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'fisheye' ) ) {
 	// Default Preferences Defines
 	define ( 'FISHEYE_DEFAULT_ROWS_PER_PAGE', 5 );
 	define ( 'FISHEYE_DEFAULT_COLS_PER_PAGE', 2 );
 	define ( 'FISHEYE_DEFAULT_THUMBNAIL_SIZE', 'small' );
-	
+
 	$gBitSmarty->assign( 'FISHEYE_DEFAULT_ROWS_PER_PAGE', FISHEYE_DEFAULT_ROWS_PER_PAGE );
 	$gBitSmarty->assign( 'FISHEYE_DEFAULT_COLS_PER_PAGE', FISHEYE_DEFAULT_COLS_PER_PAGE );
 	$gBitSmarty->assign( 'FISHEYE_DEFAULT_THUMBNAIL_SIZE', FISHEYE_DEFAULT_THUMBNAIL_SIZE );
 
 	$gBitSystem->registerAppMenu( FISHEYE_PKG_NAME, $gBitSystem->getPreference('fisheye_menu_text', ucfirst( FISHEYE_PKG_DIR ) ), FISHEYE_PKG_URL.'index.php', 'bitpackage:fisheye/menu_fisheye.tpl', 'Image Galleries');
-	
+
 	include_once( FISHEYE_PKG_PATH.'FisheyeGallery.php' );
 }
 ?>
