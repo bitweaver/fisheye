f9f4b9c0c2a0e1414bfa235f4c4d08fc4bda9dbc
Christian Fowler <spider@viovio.com>
2009-01-10 22:05:35 +0000

    delete users galleries when deleting user

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -22,6 +22,27 @@ if( $gBitSystem->isPackageActive( 'fisheye' ) && $gBitUser->hasPermission( 'p_fi
 	);
 	$gBitSystem->registerAppMenu( $menuHash );
 
+	define( 'LIBERTY_SERVICE_PHOTOSHARING', 'photosharing');
+
+	$gLibertySystem->registerService( LIBERTY_SERVICE_PHOTOSHARING, FISHEYE_PKG_NAME, array(
+		'users_expunge_function' => 'fisheye_expunge_user',
+	) );
+
+	function fisheye_expunge_user( $pObject ) {
+		global $gBitDb;
+		if( !empty( $pObject->mUserId ) ) {
+			$query = "SELECT fg.`content_id` FROM `".BIT_DB_PREFIX."fisheye_gallery` fg INNER JOIN `".BIT_DB_PREFIX."liberty_content` lc ON(fg.`content_id`=lc.`content_id`) WHERE lc.`user_id`=?";
+			if( $galleries = $gBitDb->getCol( $query, array( $pObject->mUserId ) ) ) {
+				foreach( $galleries as $contentId ) {
+					$delGallery = new FisheyeGallery( NULL, $contentId );
+					if( $delGallery->load() ) {
+						$delGallery->expunge( TRUE );
+					}
+				}
+			}
+		}
+	}
+
 	include_once( FISHEYE_PKG_PATH.'FisheyeGallery.php' );
 }
 ?>
