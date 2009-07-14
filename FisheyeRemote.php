7bad867506398b0a9a9bf5bde715317aa1b4bb58
Tyler Bello <tylerbello@users.sourceforge.net>
2009-07-14 18:48:24 +0000

    Variety of fixes and album nesting support

--- FisheyeRemote.php
+++ FisheyeRemote.php
@@ -3,7 +3,7 @@
 * Gallery2 Remote support for fisheye
 *
 * @package  fisheye
-* @version  $Header: /cvsroot/bitweaver/_bit_fisheye/FisheyeRemote.php,v 1.2 2009/07/13 18:10:13 spiderr Exp $
+* @version  $Header: /cvsroot/bitweaver/_bit_fisheye/FisheyeRemote.php,v 1.3 2009/07/14 18:48:24 tylerbello Exp $
 * @author   spider <spider@steelsun.com>
 * @author   tylerbello <tylerbello@gmail.com>
 */
@@ -58,9 +58,9 @@ class FisheyeRemote {
 	// separate out pPostData and pParamhash data since some plugins can populate _POST['g2_form'] and _GET['g2_form'] differently.
 	// weird but true. ubermind is an example
     function processRequest( $pGetData, $pPostData ) {
-		
-		if(!empty($pGetData)){
-			switch ($pGetData['cmd']) {
+		$pData = array_merge($pGetData, $pPostData); //Some programs (galleryexport) pass both post and get...and the cmd can be in either get or post
+		if(!empty($pData)){
+			switch ($pData['cmd']) {
 				case 'login':
 					$response = $this->cmdLogin( $pPostData );
 					break;
@@ -129,8 +129,7 @@ class FisheyeRemote {
 
     function cmdLogin( $pParamHash ) {
 		global $gBitUser;
-	
-		$url = $gBitUser->login( $pParamHash['uname'], $pParamHash['password'] );	
+		$url = $gBitUser->login( $pParamHash['uname'], $pParamHash['password'] );
 		if( $gBitUser->isRegistered() ) {
 			$response = $this->createResponse( FEG2REMOTE_SUCCESS, 'Login successful.', array( 'server_version' => $this->getApiVersion() ) );
 		} else {
@@ -141,26 +140,30 @@ class FisheyeRemote {
 
 	// Recursively traverses a multi-dimensional array of galleries
 	function traverseGalleries( $pGalHash, &$pResponse ) {
-		static $i = 1;
+		static $i = 1; //Albums don't like being 0 indexed 
 		foreach( $pGalHash as $key=>$gallery)
 		{ 
 			if($gallery['content']['level'] != 0){
-				$gallery['content']['title']= '|'.str_repeat('-',$gallery['content']['level']).$gallery['content']['title'];
-			}	
-			$pResponse['album.name.' . $i] = $gallery['content']['gallery_id'];
+			$pResponse['album.parent.' . $i] =  $gallery['content']['cb_gallery_content_id'];
+			}
+			$pResponse['album.name.' . $i] = $gallery['content']['content_id'];
 			$pResponse['album.title.' . $i] = $this->cleanResponseValue( $gallery['content']['title'] );
+			
+			$pResponse['album.description.' . $i] = $gallery['content']['data'];
 
 			$pResponse['album.perms.add.' . $i] = 'true';
 			$pResponse['album.perms.write.' . $i] = 'true';
 			$pResponse['album.perms.del_alb.' . $i] = 'true';
 			$pResponse['album.perms.create_sub.' . $i] = 'true';
+				
+			$pResponse['album.info.extrafields.' . $i] = "Summary,Description";
+			
 			$i++;
-		
 			if( !empty( $gallery['children'] ) ) { 
 				$this->traverseGalleries($gallery['children'],$pResponse);
 			}
 		}
-		return $i;
+		return $i - 1; //Because albums don't like being 0 indexed, we must subtract one from the total album count
 	}	
 
     function cmdFetchAlbums( $pParamHash ) {
@@ -192,18 +195,25 @@ class FisheyeRemote {
 		} elseif( empty( $uploadFile ) || empty( $uploadFile['size'] ) ) {
 			$response = $this->createResponse( FEG2REMOTE_NO_FILENAME, 'No image uploaded' );
 		} else {
-			$storeHash['title'] = !empty( $pParamHash['caption'] ) ? $pParamHash['caption'] : NULL;
+			$storeHash['title'] = !empty( $pParamHash['force_filename'] ) ? $pParamHash['force_filename'] : NULL;
 			$storeHash['summary'] = !empty( $pParamHash['extrafield.Summary'] ) ? $pParamHash['extrafield.Summary'] : NULL;
 			$storeHash['edit'] = !empty( $pParamHash['extrafield.Description'] ) ? $pParamHash['extrafield.Description'] : NULL;
 
 			require_once (FISHEYE_PKG_PATH.'upload_inc.php');	
-			
-			$_REQUEST['gallery_additions'] = array($pParamHash['set_albumName']);
-			fisheye_store_upload( $uploadFile , $storeHash );
+		
+			$parentGallery = new FisheyeGallery();
+			$parentGallery = $parentGallery->lookup(array('content_id' => $pParamHash['set_albumName'] ) );
+			$parentGallery->load();
+		
 
-			$response = $this->createResponse( FEG2REMOTE_SUCCESS, 'Image added', array( 'item_name'=>$uploadFile['name'] ) );
+			$_REQUEST['gallery_additions'] = array($parentGallery->mGalleryId);
+			if(fisheye_store_upload( $uploadFile , $storeHash )){
+				$response = $this->createResponse( FEG2REMOTE_SUCCESS, 'Image added', array( 'item_name'=>$uploadFile['name'] ) );
+			} else {
+			 	$response = $this->createResponse( FEG2REMOTE_UPLOAD_PHOTO_FAIL, 'Export Failed' );
+			}
 		}
-
+		
 		return $response;
     }
 
@@ -215,12 +225,17 @@ class FisheyeRemote {
 			$pParamHash['newAlbumTitle'] = $gBitUser->getTitle()."'s Gallery";
 		}
 
-		$storeHash['title'] = $pParamHash['newAlbumTitle'];
-		$storeHash['edit']  = $pParamHash['newAlbumDesc'];
-
+		$storeHash['title'] = !empty($pParamHash['newAlbumTitle']) ? $pParamHash['newAlbumTitle'] : '';
+		$storeHash['edit']  = !empty($pParamHash['newAlbumDesc'])  ? $pParamHash['newAlbumDesc']  : '';
 		$gallery = new FisheyeGallery();	
 		$gallery->store( $storeHash );
-		$gallery->addToGalleries( array( $pParamHash['set_albumName'] ) );
+
+		if($pParamHash['set_albumName']){
+			$parentGallery = new FisheyeGallery();
+			$parentGallery = $parentGallery->lookup(array('content_id' => $pParamHash['set_albumName'] ) );
+			$parentGallery->load();
+			$gallery->addToGalleries(array($parentGallery->mGalleryId));
+		}
 
 		$response = $this->createResponse( FEG2REMOTE_SUCCESS, 'Gallery created', array( 'album_name' => $storeHash['title'] ) );
 
@@ -241,8 +256,7 @@ class FisheyeRemote {
 		// Each response must contain at least the keys: status and status_text. 
 		$ret['status'] = $this->cleanResponseValue( $pStatus );
 		// translate the text response for i18n
-		$ret['status_text'] = $this->cleanResponseValue( tra( $pStatus ) );
-
+		$ret['status_text'] = $this->cleanResponseValue( tra( $pStatusText ) );
 		// tack on any additional responses
 		if( !empty( $pExtra ) && is_array( $pExtra ) ) {
 			foreach( $pExtra as $k => $value ) {
--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 * Gallery2 Remote support for fisheye
 *
 * @package  fisheye
-* @version  $Header: /cvsroot/bitweaver/_bit_fisheye/main.php,v 1.2 2009/07/13 18:10:13 spiderr Exp $
+* @version  $Header: /cvsroot/bitweaver/_bit_fisheye/main.php,v 1.3 2009/07/14 18:48:24 tylerbello Exp $
 * @author   spider <spider@steelsun.com>
 * @author   tylerbello <tylerbello@gmail.com>
 */
@@ -28,12 +28,12 @@ require_once( FISHEYE_PKG_PATH.'FisheyeGallery.php' );
 
 $gFisheyeRemote = new FisheyeRemote();
 
-
 if( !empty( $_REQUEST['g2_form'] ) ){
-	$gFisheyeRemote->processRequest( $_GET['g2_form'], (!empty( $_POST['g2_form'] ) ? $_POST['g2_form'] : array()) );
+	$gFisheyeRemote->processRequest( (!empty( $_GET['g2_form'] ) ? $_GET['g2_form'] : array()), (!empty( $_POST['g2_form'] ) ? $_POST['g2_form'] : array()) );
 } elseif( !empty( $_REQUEST['g2_itemId'] ) ) {
 	//If we don't have g2_form, they must be asking the gallery to be opened upon export completion
-	$gallery = new FisheyeGallery( $_REQUEST['g2_itemId'] );
+	$gallery = new FisheyeGallery();
+	$gallery = $gallery->lookup(array('content_id' => $_REQUEST['g2_itemId'] ));
 	bit_redirect( $gallery->getDisplayUrl() );
 }
 
