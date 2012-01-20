3664b46e812748fbc08d30b8b3b01e3d2029cfdf
spiderr <spider@viovio.com>
2012-01-20 14:49:28 -0500

    comment fisheye_show_public_on_upload support in upload.php

--- upload.php
+++ upload.php
@@ -49,7 +49,7 @@ $getHash = array(
 if( $gBitSystem->isFeatureActive( 'fisheye_show_all_to_admins' ) && $gBitUser->hasPermission( 'p_fisheye_admin' ) ) {
 	unset( $getHash['user_id'] );
 } elseif( $gBitSystem->isFeatureActive( 'fisheye_show_public_on_upload' ) ) {
-	$getHash['show_public'] = TRUE;
+//	$getHash['show_public'] = TRUE; THis should be handled with a content_status, disabled for now
 }
 
 $galleryTree = $gContent->generateList( $getHash,  array( 'name' => "gallery_id", 'id' => "gallerylist", 'item_attributes' => array( 'class'=>'listingtitle'), 'radio_checkbox' => TRUE, ), true );
