2543fb5456041b7c17bbeb29153f01ee25b7c36f
spiderr <spiderr@bitweaver.org>
2021-02-13 22:02:11 -0500

    update package_path in bit_setup_inc

--- includes/bit_setup_inc.php
+++ includes/bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem, $gBitUser, $gBitSmarty, $gBitThemes;
 
 $registerHash = array(
 	'package_name' => 'fisheye',
-	'package_path' => dirname( __FILE__ ).'/',
+	'package_path' => dirname( dirname( __FILE__ ) ).'/',
 	'homeable' => TRUE,
 );
 $gBitSystem->registerPackage( $registerHash );
